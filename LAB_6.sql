6a61a24e9f322cb9f6ff189131c850c0e88564e0
igilm <asanaliigilmanov@gmail.com>
2024-11-04 20:34:12 +0500

    lab

--- LAB_6.sql
+++ LAB_6.sql
@@ -11,10 +11,10 @@ CREATE TABLE locations
 
 CREATE TABLE departments
 (
-    department_id    SERIAL PRIMARY KEY,
+    department_id   SERIAL PRIMARY KEY,
     department_name VARCHAR(50) UNIQUE,
-    budget           INTEGER,
-    location_id      INTEGER REFERENCES locations
+    budget          INTEGER,
+    location_id     INTEGER REFERENCES locations (location_id)
 );
 
 CREATE TABLE employees
@@ -25,35 +25,37 @@ CREATE TABLE employees
     email         VARCHAR(50),
     phone_number  VARCHAR(20),
     salary        INTEGER,
-    department_id INTEGER REFERENCES departments
+    department_id INTEGER REFERENCES departments (department_id)
 );
 
-SELECT first_name, last_name, department_name, departments.department_id
-FROM employees
-LEFT JOIN departments
-ON employees.department_id = departments.department_id;
+SELECT e.first_name, e.last_name, d.department_name, d.department_id
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id;
 
 SELECT e.first_name, e.last_name, e.department_id, d.department_name
 FROM employees e
 LEFT JOIN departments d
-ON e.department_id = d.department_id BETWEEN 40 AND 80;
+ON e.department_id = d.department_id
+WHERE e.department_id IN (40, 80);
 
-SELECT first_name, last_name, d.department_name, l.city, l.state_province
+SELECT e.first_name, e.last_name, d.department_name, l.city, l.state_province
 FROM employees e
 LEFT JOIN departments d
-    ON e.department_id = d.department_id
+ON e.department_id = d.department_id
 LEFT JOIN locations l
 ON d.location_id = l.location_id;
 
-SELECT department_name
+SELECT d.department_name
 FROM departments d
 LEFT JOIN employees e
-ON d.department_id = e.department_id;
+ON d.department_id = e.department_id
 
 SELECT e.first_name, e.last_name, e.department_id, d.department_name
 FROM employees e
 LEFT JOIN departments d
 ON e.department_id = d.department_id;
+
 
 
 
